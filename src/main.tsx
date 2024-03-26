38e5db5bfb87a6084cec3c52a0459a6e979e11de
TheArcus02 <m.urbaniak.inf@gmail.com>
2024-03-26 11:44:41 +0100

    ui setup, essential libs

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemeProvider } from './providers/theme-provider.tsx'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+        <App />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
