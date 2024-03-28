88fb354deabbe31fd30c443421d361d02ffef866
TheArcus02 <m.urbaniak.inf@gmail.com>
2024-03-28 11:43:58 +0100

    data-table-colum-header story improvment, packange manager changed to npm

--- src/components/datatable/data-table-column-header/data-table-column-header.stories.tsx
+++ src/components/datatable/data-table-column-header/data-table-column-header.stories.tsx
@@ -5,7 +5,7 @@ import {
   getCoreRowModel,
   useReactTable,
 } from '@tanstack/react-table'
-import { mockColumns, mockUsers } from '@/lib/mocks/datatable.mock'
+import { mockUsers } from '@/lib/mocks/datatable.mock'
 import {
   TableRow,
   TableHead,
@@ -20,47 +20,71 @@ const meta: Meta<typeof DataTableColumnHeader> = {
   parameters: {
     layout: 'centered',
     controls: {
-      disabled: true,
+      expanded: true,
     },
+  },
+  argTypes: {
+    title: {
+      control: {
+        type: 'text',
+      },
+    },
+    column: {
+      control: false,
+    },
+  },
+  args: {
+    title: 'name',
   },
 }
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-const TableHeaders = () => {
-  const table = useReactTable({
-    data: mockUsers,
-    columns: [mockColumns[0]],
-    getCoreRowModel: getCoreRowModel(),
-  })
-
-  return (
-    <Table>
-      <TableHeader>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <TableRow key={headerGroup.id}>
-            {headerGroup.headers.map((header) => {
-              return (
-                <TableHead key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext(),
-                      )}
-                </TableHead>
-              )
-            })}
-          </TableRow>
-        ))}
-      </TableHeader>
-    </Table>
-  )
-}
-
 const Template: Story = {
-  render: () => <TableHeaders />,
+  render: function Render(args) {
+    const table = useReactTable({
+      data: mockUsers,
+      columns: [
+        {
+          accessorKey: args.title || 'name',
+          header: ({ column }) => {
+            console.log(column)
+            return (
+              <DataTableColumnHeader
+                column={column}
+                title={args.title || 'name'}
+              />
+            )
+          },
+        },
+      ],
+      getCoreRowModel: getCoreRowModel(),
+    })
+
+    return (
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                )
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+      </Table>
+    )
+  },
 }
 
 export const Default: Story = {
